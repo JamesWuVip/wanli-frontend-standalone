1755ad77da35abc1858325f8614c6b031a9fefde
JamesWuVip <156403378+JamesWuVip@users.noreply.github.com>
2025-08-19 16:15:34 +0800

    推送剩余配置文件和入口文件

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue', 'vue-router', 'pinia']
+        }
+      }
+    }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: parseInt(process.env.PORT || '4173'),
+    strictPort: true
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
+})
